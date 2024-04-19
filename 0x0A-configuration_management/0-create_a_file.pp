92c6fa1efcd21f7decb5f6ffc8cecbc35659a5ff
Sarah-Gad <sarahgad298@gmail.com>
2024-04-19 16:58:53 -0700

    completed task n.0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest will create a file.
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This directory contains my tasks for the 0x0A-configuration_management project.
