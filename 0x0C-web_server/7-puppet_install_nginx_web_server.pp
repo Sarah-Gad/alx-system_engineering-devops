7dda2c31ead41d0990bde3b9a65d83b3d888b401
Sarah-Gad <sarahgad298@gmail.com>
2024-04-23 15:26:05 -0700

    edit .-.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,24 @@
-# This manifest will automate the configiration process of nginx
+# Setup New Ubuntu server with nginx
 
-exec { 'updating sys':
-  command  => '/usr/bin/apt-get update',
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-package { 'installing nginx':
-  ensure   => 'installed',
-  require  => Exec['updating sys']
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
-  content  => 'Hello World!'
+	content => 'Hello World!'
 }
 
-exec {'redirecting':
-  command  => 'sed -i "24i\       rewrite ^/redirect_me https://www.google.com/ permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+exec {'redirect_me':
+	command => 'sed -i "24i\       rewrite ^/redirect_me https://www.google.com/ permanent;" /etc/nginx/sites-available/default'
+	provider => 'shell'
 }
 
-service {'running_nginx':
-  ensure   => running,
-  require  => Package['installing nginx']
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
