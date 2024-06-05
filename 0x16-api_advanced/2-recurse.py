4126731343bfc88810eb12d8d6d1c3e92bf025c4
Sarah Gad <sarahgad298@gmail.com>
2024-06-05 05:12:34 +0300

    removed list_of_titles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,6 @@ import requests
 def recurse(subreddit, hot_list=[], after="", number=0):
     """This fucntion will returns a list containing the titles
     of all hot articles using recursive function"""
-    list_of_titles = []
     rd_endpoint = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     rd_headers = {
         "User-Agent": "windows, for 0x16-api_advanced project by Sarah-Gad "
@@ -18,8 +17,8 @@ def recurse(subreddit, hot_list=[], after="", number=0):
     }
     respo = requests.get(
         rd_endpoint,
-        params=rd_params,
         headers=rd_headers,
+        params=rd_params,
         allow_redirects=False)
     if respo.status_code == 404:
         return None
@@ -27,7 +26,7 @@ def recurse(subreddit, hot_list=[], after="", number=0):
     after = parsed_repos.get("after")
     number += parsed_repos.get("dist")
     for one in parsed_repos.get("children"):
-        list_of_titles.append(one.get("data").get("title"))
+        hot_list.append(one.get("data").get("title"))
     if after is not None:
         return recurse(subreddit, hot_list, after, number)
-    return list_of_titles
+    return hot_list
