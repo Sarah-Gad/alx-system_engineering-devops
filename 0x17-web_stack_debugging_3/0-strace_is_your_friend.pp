4729e96e38f99e879ff694046be60a0c38fd4332
Sarah Gad <sarahgad298@gmail.com>
2024-06-06 06:05:33 +0300

    0_0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,11 @@
 package { 'ruby':
   ensure => installed}
 
+exec { 'install_puppet_lint':
+  command => 'gem install puppet-lint -v 2.1.1',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  require => Package['ruby']}
+
 exec {'editing text file':
   command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'}
