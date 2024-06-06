987252ab48fa538859326087f9d4a4170334f446
Sarah Gad <sarahgad298@gmail.com>
2024-06-06 06:04:17 +0300

    .-.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,7 @@
+
+package { 'ruby':
+  ensure => installed}
+  
 exec {'editing text file':
   command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'}
